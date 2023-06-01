dbdb3fcc55bb688567bfff2a257178d56b9de875
sharath-98 <sharathchander.p@gmail.com>
2023-06-01 14:38:13 -0500

    logic for adding points

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QuestionService } from '../service/question.service';
 
 @Component({
   selector: 'app-question',
@@ -8,11 +9,42 @@ import { Component, OnInit } from '@angular/core';
 export class QuestionComponent implements OnInit {
 
   name: string = "";
+  questionList : any = [];
+  currentQuestion : number = 0;
+  points: number = 0;
+  counter: number = 60; 
+  numCorrectAns: number = 0;
+  numIncorrectAns: number = 0;
 
-  constructor() { }
+  constructor(private questionService : QuestionService) { }
 
   ngOnInit(): void {
     this.name = localStorage.getItem('name')!;
+    this.getAllQuestions();
+  }
+
+  getAllQuestions(){
+    this.questionService.getQuestionJSON().subscribe(res => this.questionList = res.questions);
+  }
+
+  NextQuestion(){
+    this.currentQuestion++;
+  }
+
+  PrevQuestion(){
+    if(this.currentQuestion > 0)
+      this.currentQuestion--;
+  }
+
+  CheckAnswer(questionNumber:number, option:any){
+    if(option.correct){
+      this.points += 10;
+      this.numCorrectAns++;
+    } else{
+      this.points-=10;
+      this.numIncorrectAns++;
+    }
+    this.currentQuestion++;
   }
 
 }
